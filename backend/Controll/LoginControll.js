77a99ad7076f2c8fba01016cc644b95eb377a008
abdirhman615 <abdirhman615@gmail.com>
2024-02-02 00:04:21 +0300

    add login

--- backend/Controll/LoginControll.js
+++ backend/Controll/LoginControll.js
@@ -15,6 +15,7 @@ const loginRouter = async (req, res) => {
     // find user data
     const usergetdata = await UserModal.findOne({
         username: req.body.username,
+        userStatus:'active'
 
     });
     if (!usergetdata)
@@ -38,7 +39,7 @@ const loginRouter = async (req, res) => {
       {
         id: usergetdata._id,
         username: usergetdata.username,
-        Role:usergetdata.Role
+        // Role:usergetdata.Role
       },
      "acbfa14fb74b48e273b6a4e911ed9fd7a9f5a3355ceda4ac0b68fa42b2527097niofh89nnspjfhusf"
     );
@@ -51,54 +52,76 @@ const loginRouter = async (req, res) => {
     res.status(400).send(error.message);
   }
 
+ 
+
+}
+//   const { error } = LoginValidate(req.body)
+//   if (error) return res.send(error.message)
+//   try {
+//   const user= await UserModal.findOne({username:req.body.username,userStatus:'active'})
+//   if(!user) return res.status(404).send({Error: 'User not found'});
+//   const CheckPassword = await bcrypt.compare(req.body.Password,user.Password)
+//   if(!CheckPassword) return res.status(404).send({Error:'Invalid password'});
+
+//   const token = jwt.sign({username:user.username,id:user._id})
+
+//   return res.status(200).send({Token:token,username:user.username +' '+'Loged in'})
+//   } catch (error) {
+//       res.status(401).send(error.message)
+      
+      
+//   }
+  
+
+// }
   
   
-  try {
-    // validation
-    const { error } = STDLoginValidate(req.body);
-    if (error) return res.status(449).send(error.message);
+  // try {
+  //   // validation
+  //   const { error } = STDLoginValidate(req.body);
+  //   if (error) return res.status(449).send(error.message);
 
-    // find user data
-    const SDTgetdata = await StudentModal.findOne({
-        Email: req.body.Email,
-    });
-    if (!SDTgetdata)
-      return res.status(401).send({
-        status: false,
-        message: 'Email or password is incorrect',
-      });
+  //   // find user data
+  //   const SDTgetdata = await StudentModal.findOne({
+  //       Email: req.body.Email,
+  //   });
+  //   if (!SDTgetdata)
+  //     return res.status(401).send({
+  //       status: false,
+  //       message: 'Email or password is incorrect',
+  //     });
 
-    // check password
-    const stdcheckpass = await bcrypt.compare(
-      req.body.STD_Pass,
-      SDTgetdata.STD_Pass
-    );
-    if (!stdcheckpass)
-      return res.status(401).send({
-        status:false,
-        message: 'Email or password is incorrect',
-      });
-    // token using jwt
-    const token = jwt.sign(
-      {
-        id: SDTgetdata._id,
-        Email: SDTgetdata.Email,
+  //   // check password
+  //   const stdcheckpass = await bcrypt.compare(
+  //     req.body.STD_Pass,
+  //     SDTgetdata.STD_Pass
+  //   );
+  //   if (!stdcheckpass)
+  //     return res.status(401).send({
+  //       status:false,
+  //       message: 'Email or password is incorrect',
+  //     }); 
+  //   // token using jwt
+  //   const token = jwt.sign(
+  //     {
+  //       id: SDTgetdata._id,
+  //       Email: SDTgetdata.Email,
        
-      },
-     "acbfa14fb74b48e273b6a4e911ed9fd7a9f5a3355ceda4ac0b68fa42b2527097niofh89nnspjfhusf"
-    );
-    res.status(200).header('token', token).json({
-      status: true,
-      message: 'successfully logged in',
-      token: token,
-    });
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+  //     },
+  //    "acbfa14fb74b48e273b6a4e911ed9fd7a9f5a3355ceda4ac0b68fa42b2527097niofh89nnspjfhusf"
+  //   );
+  //   res.status(200).header('token', token).json({
+  //     status: true,
+  //     message: 'successfully logged in',
+  //     token: token,
+  //   });
+  // } catch (error) {
+  //   res.status(400).send(error.message);
+  // }
 
 
 
    
-};
+
 
 module.exports = {loginRouter};
